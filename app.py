ac72b819f8e63071e7bf2426c444e2d18c190403
Diego Wal Cunha Hernandes <d.cunha.hernandes@avanade.com>
2023-11-17 10:39:27 -0300

    :up: cretaion of forms crsf

--- app.py
+++ app.py
@@ -18,9 +18,5 @@ def usuarios():
 def login():
     return render_template('login.html')
 
-@app.route("/cadastro")
-def cadastro():
-    return render_template('cadastro.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
