0c71d5c43d0d94ef6c06815df1bb7a333d50ffbd
Diego Wal Cunha Hernandes <d.cunha.hernandes@avanade.com>
2023-11-17 15:25:17 -0300

    :up: applying bootstrap to the form

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dcf5c2e692aa78a1e79f7f74a42863eb'
 
 @app.route("/")
 def home():
@@ -16,7 +20,9 @@ def usuarios():
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class FormCriarConta(FlaskForm):
@@ -12,4 +12,5 @@ class FormCriarConta(FlaskForm):
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
