f24e1114c764f121141d82aa5053fa02786a3604
Diego Wal Cunha Hernandes <d.cunha.hernandes@avanade.com>
2023-11-18 16:16:53 -0300

    :up: creation of form validation and alert messages using flash

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 
 
@@ -18,10 +18,19 @@ def contato():
 def usuarios():
     return render_template('usuarios.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucessono e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada para o email: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
