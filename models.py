c455f491b49db4d12ecad4cb99f8288eb2ce323b
Diego Wal Cunha Hernandes <d.cunha.hernandes@avanade.com>
2023-11-21 09:15:28 -0300

    :up: creating tables in the database

---
+++ models.py
@@ -0,0 +1,15 @@
+from main import database
+from datetime import datetime
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False, unique=True)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+    foto_perfil = database.Column(database.String, default='default.jpg', nullable=False)
+
+class Post(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    titulo = database.Column(database.String, nullable=False)
+    corpo = database.Column(database.Tex, nullable=False)
+    data_criacao = database.Column(database.DataTime, nullable=False, default=datetime.utcnow)
