4ae8608b0c845b4d6853e5d804192c8de296da82
Diego Wal Cunha Hernandes <d.cunha.hernandes@avanade.com>
2023-11-21 10:29:07 -0300

    :up: creating relationship between tables in the database

--- models.py
+++ models.py
@@ -7,9 +7,12 @@ class Usuario(database.Model):
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
     foto_perfil = database.Column(database.String, default='default.jpg', nullable=False)
+    posts = database.relationship('Post', backref='autor'. lazy=True)
+    profissao = database.Column(database.String, nullable=False, default='Não Informado')
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     titulo = database.Column(database.String, nullable=False)
     corpo = database.Column(database.Tex, nullable=False)
     data_criacao = database.Column(database.DataTime, nullable=False, default=datetime.utcnow)
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id')), nullable=False
