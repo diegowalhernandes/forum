4f7276729c900585e34f29a783bb35b20256c8f0
Diego Wal Cunha Hernandes <d.cunha.hernandes@avanade.com>
2023-11-21 14:52:47 -0300

    :up: creating and testing database

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dcf5c2e692aa78a1e79f7f74a42863eb'
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///forum.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
+
 
 database = SQLAlchemy(app)
 
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from main import database
+from app import database
 from datetime import datetime
 
 class Usuario(database.Model):
@@ -7,12 +7,12 @@ class Usuario(database.Model):
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
     foto_perfil = database.Column(database.String, default='default.jpg', nullable=False)
-    posts = database.relationship('Post', backref='autor'. lazy=True)
+    posts = database.relationship('Post', backref='autor', lazy=True)
     profissao = database.Column(database.String, nullable=False, default='Não Informado')
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     titulo = database.Column(database.String, nullable=False)
-    corpo = database.Column(database.Tex, nullable=False)
-    data_criacao = database.Column(database.DataTime, nullable=False, default=datetime.utcnow)
-    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id')), nullable=False
+    corpo = database.Column(database.Text, nullable=False)
+    data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
