4dcf486bdc22bfb46523fbad1288626b0aa45518
Diego Wal Cunha Hernandes <d.cunha.hernandes@avanade.com>
2023-11-27 09:24:08 -0300

    :up: creation if functionality for login verification

--- forum/routes.py
+++ forum/routes.py
@@ -2,6 +2,10 @@ from flask import render_template, redirect, url_for, flash, request
 from forum import app, database, bcrypt
 from forum.forms import FormLogin, FormCriarConta
 from forum.models import Usuario
+from flask_login import login_user
+
+# lista de usuarios
+
 
 @app.route("/")
 def home():
@@ -15,14 +19,21 @@ def contato():
 def usuarios():
     return render_template('usuarios.html')
 
+
+
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
-
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f'Login feito com sucessono e-mail: {form_login.email.data}', 'alert-success')
-        return redirect(url_for('home'))
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            flash(f'Login feito com sucessono e-mail: {form_login.email.data}', 'alert-success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Falha no Login. E-mail ou senha Incorreto', 'alert-danger')
 
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
