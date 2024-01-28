f850438d509793e0bb278ab83589414930df38ce
Diego Hernandes <diegowalhernandes@gmail.com>
2024-01-28 00:33:09 -0300

    :up: finishing features delete post

--- forum/routes.py
+++ forum/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, abort
 from forum import app, database, bcrypt
 from forum.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
 from forum.models import Usuario, Post
@@ -135,4 +135,16 @@ def exibir_post(post_id):
     else:
         form = None
     return render_template('post.html', post=post, form=form)
+
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post Excluido com Sucesso', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
 
